e51c663524eb9c28825f9464f0569bed1293ed26
Angel Zamora <23302019@utguaymas.edu.mx>
2026-07-28 20:47:02 -0700

    Visualizacion de pestañas

--- database/supabase_client.py
+++ database/supabase_client.py
@@ -1,15 +1,28 @@
 from supabase import create_client
 import streamlit as st
 
-
-# =====================================================
-# CLIENTE SUPABASE
-# =====================================================
-
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+
+st.write("URL:", SUPABASE_URL)
 
 supabase = create_client(
     SUPABASE_URL,
     SUPABASE_KEY
 )
+
+try:
+    prueba = (
+        supabase
+        .table("reportes")
+        .select("*")
+        .limit(1)
+        .execute()
+    )
+
+    st.success("Conexión correcta")
+    st.write(prueba.data)
+
+except Exception as e:
+
+    st.error(e)
